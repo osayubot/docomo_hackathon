70d2fe87aa81fcdd901adb48f40cca1273f06b6b
Daiki Io <nirasyuu@gmail.com>
2024-09-05 18:21:49 +0900

    途中

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -29,10 +29,10 @@ const Sidebar = ({
         <DrawerHeader borderBottomWidth="1px">メニュー</DrawerHeader>
         <DrawerBody>
           <Text my={1}>
-            <Link href="/about">kikitoの家電ガチャって何？</Link>
+            <Link href="https://www.docomo.ne.jp/service/kikito/">kikitoって何？</Link>
           </Text>
           <Text my={1}>
-            <Link href="/home">ホーム</Link>
+            <Link href="/home">どんな商品があるか</Link>
           </Text>
         </DrawerBody>
       </DrawerContent>
