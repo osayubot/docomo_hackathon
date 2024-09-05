5b5639e03bd96a2727266ed29ae2a07440e58565
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2024-09-05 11:13:23 +0900

    fix: image size

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -32,6 +32,8 @@ const Page = () => {
             src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
             alt="Green double couch with wooden legs"
             borderRadius="lg"
+            maxW={"80%"}
+            marginX={"auto"}
           />
           <Stack mt="6" spacing="3">
             <Heading size="md">
