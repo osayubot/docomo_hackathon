1536f8d2e0da208419631d2de760cd50b78defb0
microglia <aricchi0208@icloud.com>
2024-09-05 16:09:42 +0900

    x

--- src/libs/recommend.ts
+++ src/libs/recommend.ts
@@ -49,7 +49,7 @@ const fetchRecommendCategory = async ({
   // このカテゴリをもとに、prisma で作成した Items テーブル から、予算(budget)に一致するアイテムをとってくる
   //
 
-  const items = await prisma.items.findMany({
+  const items = await prisma.items.findFirst({
       where: {
         category: category,
         initialPrice: { lte: budget },
